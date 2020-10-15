1131875cc574a2d4bb6b856faf980a3de9d77e4e
Hernán Cano <hernancano2014@gmail.com>
2020-10-15 15:45:55 -0300

    Initial Commit

---
+++ src/main/java/com/example/demo/ApiAlumnoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAlumnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAlumnoApplication.class, args);
+	}
+
+}
