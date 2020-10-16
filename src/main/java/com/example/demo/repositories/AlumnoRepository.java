356daa4a31b95747e29285caf684dc47667fcacd
Hernán Cano <hernancano2014@gmail.com>
2020-10-16 15:53:38 -0300

    Error solucionado

--- src/main/java/com/example/demo/repositories/AlumnoRepository.java
+++ src/main/java/com/example/demo/repositories/AlumnoRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repositories;
 
+import com.example.demo.entities.Alumno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
