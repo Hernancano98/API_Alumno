de36a7a8ef0da4932d7bee71a5b2a328d3cad0f4
Hernán Cano <hernancano2014@gmail.com>
2020-10-16 15:04:39 -0300

    Update services

--- src/main/java/com/example/demo/services/BaseService.java
+++ src/main/java/com/example/demo/services/BaseService.java
@@ -1,2 +1,11 @@
-package com.example.demo.services;public interface BaseService {
+package com.example.demo.services;
+
+import java.util.List;
+
+public interface BaseService<E> {
+    public List<E> findAll() throws Exception;
+    public E findById(Long Id) throws Exception;
+    public E save(E entity) throws Exception;
+    public E update(Long id, E entity) throws Exception;
+    public boolean delete(Long id) throws Exception;
 }
