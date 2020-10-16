b29758eb12181c43717b870ce133814b2ec86292
Enzito <enzoavena1@gmail.com>
2020-10-16 14:58:19 -0300

    entidad alumno completa

--- src/main/java/com/example/demo/entities/Alumno.java
+++ src/main/java/com/example/demo/entities/Alumno.java
@@ -1,11 +1,29 @@
 package com.example.demo.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.Date;
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Audited
+@Table(name = "alumno")
 public class Alumno implements Serializable {
+    @Column(name = "nombre")
     private String nombre;
+    @Column(name = "apellido")
     private String apellido;
+    @Column(name = "email")
     private String email;
+    @Column(name = "createAt")
     private Date createAt;
 }
